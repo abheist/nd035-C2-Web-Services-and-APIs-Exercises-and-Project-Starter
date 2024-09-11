66d83b464199acdf66af68a5bc3ddda44c2fdbf3
abheist <abhishek291093@gmail.com>
2024-09-10 21:44:55 -0300

    fixed the pricing fetch

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/api/PricingController.java
@@ -19,17 +19,24 @@ public class PricingController {
 
     /**
      * Gets the price for a requested vehicle.
+     *
      * @param vehicleId ID number of the vehicle for which the price is requested
      * @return price of the vehicle, or error that it was not found.
      */
+
+    private final PricingService pricingService;
+
+    public PricingController(PricingService pricingService) {
+        this.pricingService = pricingService;
+    }
+
     @GetMapping
     public Price get(@RequestParam Long vehicleId) {
         try {
-            return PricingService.getPrice(vehicleId);
+            return pricingService.getPrice(vehicleId);
         } catch (PriceException ex) {
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Price Not Found", ex);
         }
-
     }
 }
--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/service/PricingService.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/service/PricingService.java
@@ -1,6 +1,7 @@
 package com.udacity.pricing.service;
 
 import com.udacity.pricing.domain.price.Price;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -12,38 +13,40 @@ import java.util.stream.LongStream;
 /**
  * Implements the pricing service to get prices for each vehicle.
  */
+@Service
 public class PricingService {
 
-    /**
-     * Holds {ID: Price} pairings (current implementation allows for 20 vehicles)
-     */
-    private static final Map<Long, Price> PRICES = LongStream
-            .range(1, 20)
-            .mapToObj(i -> new Price("USD", randomPrice(), i))
-            .collect(Collectors.toMap(Price::getVehicleId, p -> p));
+    private final Map<Long, Price> PRICES;
 
-    /**
-     * If a valid vehicle ID, gets the price of the vehicle from the stored array.
-     * @param vehicleId ID number of the vehicle the price is requested for.
-     * @return price of the requested vehicle
-     * @throws PriceException vehicleID was not found
-     */
-    public static Price getPrice(Long vehicleId) throws PriceException {
-
-        if (!PRICES.containsKey(vehicleId)) {
-            throw new PriceException("Cannot find price for Vehicle " + vehicleId);
-        }
-
-        return PRICES.get(vehicleId);
+    public PricingService() {
+        PRICES = LongStream
+                .range(1, 20)
+                .mapToObj(i -> new Price("USD", randomPrice(), i))
+                .collect(Collectors.toMap(Price::getVehicleId, p -> p));
     }
 
     /**
      * Gets a random price to fill in for a given vehicle ID.
+     *
      * @return random price for a vehicle
      */
-    private static BigDecimal randomPrice() {
-        return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5))
-                .multiply(new BigDecimal(5000d)).setScale(2, RoundingMode.HALF_UP);
+    private BigDecimal randomPrice() {
+        return BigDecimal.valueOf(ThreadLocalRandom.current().nextDouble(1, 5))
+                .multiply(new BigDecimal("5000")).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * If a valid vehicle ID, gets the price of the vehicle from the stored array.
+     *
+     * @param vehicleId ID number of the vehicle the price is requested for.
+     * @return price of the requested vehicle
+     * @throws PriceException vehicleID was not found
+     */
+    public Price getPrice(Long vehicleId) throws PriceException {
+        if (!PRICES.containsKey(vehicleId)) {
+            throw new PriceException("Cannot find price for Vehicle " + vehicleId);
+        }
+        return PRICES.get(vehicleId);
     }
 
 }
